72678e7883f3d3ba422222127de188ddfa2222ac
annaklimenko <annaklimenko5248@gmail.com>
2023-05-03 14:23:12 +0300

    Создали калькулятор и действие сложение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        System.out.println(a + b);
     }
 }
